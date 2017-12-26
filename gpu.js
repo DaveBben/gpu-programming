894f9d8d5ce7269221588df3d0d12fedd0c0b23d
Dave Bennett <david.a.bennett7@gmail.com>
2017-12-26 11:49:49 -0500

    Using constants

--- gpu.js
+++ gpu.js
@@ -1,16 +1,19 @@
 const gpu = new GPU();
 
 //the length and width of the array
-const SIZE = 5000;
+const SIZE = 1000;
 
 
-const matMult  = gpu.createKernel(function (a, b, size) {
+const matMult  = gpu.createKernel(function (a, b) {
   let sum = 0;
-  for (let i = 0; i < size; i++) {
+  for (let i = 0; i < this.constants.size; i++) {
     sum += a[this.thread.y][i] * b[i][this.thread.x];
   }
   return sum;
-}).setOutput([5000, 5000]);
+ } , {
+    constants: { size: SIZE },
+    output: [SIZE,SIZE],
+  });
 
 
 //creating a 5000 X 5000 matrix
@@ -23,6 +26,6 @@ for (let i = 0; i < SIZE; i++) {
   value.push(row);
 }
 
-const result = matMult(value, value, SIZE);
+const result = matMult(value, value);
 
 console.log(result);
